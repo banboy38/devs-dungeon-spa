55f2aa2bf5d29f41fb910a1dc4c8cdb209010348
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2022-05-24 00:42:00 +0530

    Yu

--- components/navbar/styles.js
+++ components/navbar/styles.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const NavBar = styled.div`
 // background: linear-gradient(270.4deg, #78D7FF -1.04%, rgba(211, 86, 255, 0.710417) 86.22%, rgba(255, 88, 148, 0.823621) 114.24%);
 
-background : rgba(242, 242, 242, 0.3);
+background : rgba(242, 242, 242, 0.85);
 // border-radius: 20px;
 box-shadow:  -20px 20px 60px #0f0f0f,
              20px -20px 60px #151515;
-backdrop-filter : opacity(40%);
-backdrop-filter : blur(2em);
+
+backdrop-filter : blur(0.5em);
 
 min-height: 100px;
 position : fixed;
