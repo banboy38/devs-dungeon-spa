d9a149d66c14fa0d31e0911070f6135a99ffd3c7
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2022-05-24 00:51:26 +0530

    Y gap

--- components/statbar/statbar.js
+++ components/statbar/statbar.js
@@ -55,7 +55,7 @@ export default function Statbar() {
                     // `}
                 </Script>
 
-                <StatBar className='flex flex-wrap justify-around place-items-center'>
+                <StatBar className='flex flex-wrap gap-y-5 justify-around place-items-center'>
 
                     <Gola icon = {personIcon} val={100} alt="person icon" text="Members" id="members"/>
 
