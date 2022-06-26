192f50a726b9d98072445ce6d4ef173f402e27c7
Anirban Aditya Halder <nabovtl@gmail.com>
2022-06-26 14:38:31 +0530

    minor changes

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -17,23 +17,23 @@ export default function Contact(){
             <div className="mt-2 m-10 flex lg:flex-row-reverse flex-col gap-x-8 gap-y-5 text-[#121212]">
 
                 <div className="basis-1/2 flex place-items-center mb-5">
-                    <Image src={Map} quality="100%"></Image>
+                    <Image src={Map} quality="100%" alt="purple geometric world-map"></Image>
                 </div>
 
                 <form id="myForm" name="email-form" data-name="Email Form" className="basis-1/2 flex flex-col  gap-y-5">
                     
                     <div className="">
-                        <div className="mb-3 text-[#f2f2f2]"><label for="Full-Name">Name</label></div>
+                        <div className="mb-3 text-[#f2f2f2]"><label htmlFor="Full-Name">Name</label></div>
                         <input type="text" className="rounded-sm h-[2rem] w-full" maxLength={32} name="Full-Name" data-name="Full Name" placeholder=" Full Name " id=" Full-Name" required="true"></input>
                     </div>
                     
                     <div>
-                        <div className="mb-3 text-[#f2f2f2]"><label for="Email">Email</label></div>
+                        <div className="mb-3 text-[#f2f2f2]"><label htmlFor="Email">Email</label></div>
                         <input type="email" className="rounded-sm h-[2rem] w-full" maxLength={32} name="Email" data-name="Email" placeholder=" E-mail" id="Email" required="true"></input>
                     </div>
                     
                     <div>
-                        <div className="mb-3 text-[#f2f2f2]"><label for="Message">Message</label></div>
+                        <div className="mb-3 text-[#f2f2f2]"><label htmlFor="Message">Message</label></div>
                         <textarea placeholder=" Message..." className="rounded-sm w-full" rows={5} maxLength={5000} id="Message" name="Message" data-name="Message" required="true" ></textarea>
                     </div>
                     
